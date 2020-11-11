4ccef3d994ede3dfd9166e7ce5c0c142d23f5844
chutting <tingting.chu@thoughtworks.com>
2020-11-11 16:26:17 +0800

    [Chu] feat: complete the practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = (input) => {
+    const dataArr = [];
+    const {data, column} = input;
+    data.forEach(info => {
+        const obj = {};
+        for(let colIndex = 0 ; colIndex < column.length; colIndex++ ) {
+            obj[column[colIndex].name] = info[colIndex]
+        }
+        dataArr.push(obj);
+    });
+    return dataArr;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,20 @@
-const inject
+const inject = (items, sections) => {
+    const totalLength = items.length + sections.length;
+    const resultArr = [];
+    let itemsIndex = 0;
+    
+    for(let i = 0; i < totalLength; i++) {
+        let sectionPosition = sections.findIndex(section => section.index === itemsIndex) ;
+
+        if (sectionPosition !== -1) {
+            resultArr.push(sections[sectionPosition].content);
+            sections.splice(sectionPosition, 1);
+        } else {
+            resultArr.push(items[itemsIndex]);
+            itemsIndex++;
+        }
+    }  
+    return resultArr;
+}
+
 export { inject };
